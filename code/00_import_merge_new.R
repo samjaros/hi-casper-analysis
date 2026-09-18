9d7bb44be4b613ec79af31d90487ca60cffad54a
Sam Jaros <Samuel.Jaros.NSW@doh.hawaii.gov>
2026-09-18 13:13:40 -1000

    Initial push of code to clean CASPER data for Kona 2026 survey

---
+++ code/00_import_merge_new.R
@@ -0,0 +1,141 @@
+# 0_import_clean_merge_data.R
+
+# Import and do initial cleaning/formatting of CASPER dataset
+#   - Rename columns
+#   - Calculate weighting
+# Merge new year data with previous data
+
+
+# Raw data comes from epicollect
+
+library(tidyverse)
+
+# Options ======================================================================
+total.hh.sample <- 4054 # Number of households in this year's sampling frame
+total.clusters <- 30
+# 2026 W Hawaii - using number from tax maps & ArcGIS, not census
+
+# Import Data ==================================================================
+casper_import_raw <- read_csv("./raw_data/form-1__2026-west-hawaii-casper.csv")
+
+# Rename Columns ===============================================================
+casper_2026 <- casper_import_raw %>%
+
+  # Title only holds redundant info
+  select(-title) %>%
+  
+  # Rename variables from start of question to relevant subjects
+  rename(
+    # Survey variables
+    surv_id = `ec5_uuid`,                            # Universal ID
+    surv_datetime = `created_at`,                    # When survey was started
+    surv_upload_datetime = `uploaded_at`,            # When survey was saved to cloud
+    surv_surveyor = `2_Surveyor_Name`,                 # Surveyor that collected data
+    surv_cluster = `3_Cluster_Number`,                 # Cluster for this survey
+    surv_survey_num = `4_Survey_Number`,               # Survey number within this cluster
+    # Demographic variables
+    demo_n_hh = `6_Including_yourself`,              # Q1 Total HH members
+    demo_n_u2 = `7_How_many_of_those_`,              # Q1 <2y
+    demo_n_2to17 = `8_How_many_of_those_`,           # Q1 2-17
+    demo_n_18to64 = `9_How_many_of_those_`,          # Q1 18-64
+    demo_n_65p = `10_How_many_of_those`,             # Q1 >65
+    demo_own_rent = `11_Does_your_househo`,          # Q2 Own/Rent
+    demo_own_rent_other = `12_If_other_please_s`,    # Q2o Own/Rent Other  
+    demo_language = `13_What_languages_ar`,          # Q3 Language
+    demo_language_other = `14_If_other_what_lan`,    # Q3o Language Other
+    # Natural disaster experience
+    NDexp_affected = `15_Has_your_househol`,         # Q4a HH affected by disaster
+    NDexp_type = `16_What_type_of_natu`,             # Q4b disaster type
+    NDexp_type_other = `17_Please_Specify`,          # Q4bo disaster type other
+    NDexp_displaced = `18_Within_the_past_2`,        # Q4c HH displaced
+    NDexp_displaced_days = `19_For_how_many_days`,   # Q4cn HH displaced days count
+    NDexp_damage = `20_How_would_you_des`,           # Q4d home damaged
+    NDexp_prepared = `21_After_the_most_re`,         # Q5 Is HH prepared
+    NDexp_safe = `22_When_considering_`,             # Q6 Is HH safe
+    NDexp_not_safe = `23_Please_Specify`,            # Q6o How is HH not safe
+    # Evacuation
+    evac_chronic_disease = `25_Chronic_disease_r`,   # Q7a HH chronic disease
+    evac_phys_disease = `26_Physical_or_devel`,      # Q7b HH physical disability
+    evac_ment_disease = `27_Mental_health_con`,      # Q7c HH mental disease
+    evac_assist = `28_Does_your_househo`,            # Q8a HH needs assistance
+    evac_assist_need = `29_If_yes_please_des`,       # Q8ao HH what assist needed
+    evac_assist_org = `30_If_yes_are_you_aw`,        # Q8b aware of community org to help
+    evac_comm_plan = `32_Communication_pla`,         # Q9a have comm plan
+    evac_meeting_place = `33_Designated_meetin`,     # Q9b have meeting place
+    evac_impt_docs = `34_Copies_of_importa`,         # Q9c have important docs
+    evac_hur_cat1 = `36_Category_1_7495_m`,          # Q10a where shelter C1
+    evac_hur_cat1_other = `37_If_other_please_s`,    # Q10ao C1 other shelter
+    evac_hur_cat2 = `38_Category_2_96110_`,          # Q10b where shelter C2
+    evac_hur_cat2_other = `39_If_other_please_s`,    # Q10bo C2 other shelter
+    evac_hur_cat3 = `40_Category_3_111129`,          # Q10c where shelter C3
+    evac_hur_cat3_other = `41_If_other_please_s`,    # Q10co C3 other shelter
+    evac_hur_cat4 = `42_Category_4_130156`,          # Q10d where shelter C4
+    evac_hur_cat4_other = `43_If_other_please_s`,    # Q10do C4 other shelter
+    evac_hur_cat5 = `44_Category_5_157_mp`,          # Q10e where shelter C5
+    evac_hur_cat5_other = `45_If_other_please_s`,    # Q10eo C5 other shelter
+    evac_tsunami_zone = `46_Is_your_household`,      # Q11 Is HH in tsunami evac zone
+    evac_prevent_fire = `47_Has_your_househol`,      # Q12 Has HH prevent wildfire
+    evac_main_barrier = `48_What_main_barrier`,      # Q13 Main barrier to evac
+    evac_barrier_other = `49_If_other_please_s`,     # Q13o Other barrier
+    evac_main_info_source = `50_What_is_your_hous`,  # Q14 Main source of info
+    evac_info_source_other = `51_If_other_please_s`, # Q14o Other info source
+    evac_alert_signup = `52_Have_you_or_anyon`,      # Q15 HH sign up for alerts
+    # Supplies
+    supp_aware_14d = `53_Is_your_household`,         # Q16 aware of 14 days food/water/med
+    supp_water_3d = `54_Does_your_househo`,          # Q17a water 3 days
+    supp_water_7d = `55_Does_your_househo`,          # Q17b water 7 days
+    supp_water_14d = `56_Does_your_househo`,         # Q17c water 14 days
+    supp_food_3d = `57_Does_your_househo`,           # Q18a food 3 days
+    supp_food_7d = `58_Does_your_househo`,           # Q18b food 7 days
+    supp_food_14d = `59_Does_your_househo`,          # Q18c food 14 days
+    supp_need_meds = `60_Does_anyone_in_yo`,         # Q19a HH need daily meds
+    supp_meds_7d = `61_If_yes_does_your_`,           # Q19b meds 7 days
+    supp_meds_14d = `62_If_yes_does_your_`,          # Q19c meds 14 days
+    supp_fire_exting = `63_Does_your_househo`,       # Q20 HH have fire extinguisher
+    supp_smoke_detect = `64_Does_your_househo`,      # Q21 HH have smoke detector
+    supp_mosquito_deet = `65_Does_your_househo`,     # Q22 HH use DEET
+    supp_mosquito_prevent = `66_How_often_does_yo`,  # Q23 prevent mosquito
+    supp_vax_important = `67_On_a_scale_from_1`,     # Q24 vaccine importance
+    # Health
+    hlth_food_ran_out = `68_During_the_past_1`,      # Q25 food ran out
+    hlth_physical = `69_On_a_scale_of_1_t`,          # Q26 physical health
+    hlth_mental = `70_On_a_scale_of_1_t`,            # Q27 mental health
+    hlth_doh_help = `71_Does_your_househo`           # Q28 can doh offer help
+  )
+
+# Weighting ====================================================================
+# TODO: Should weight be calculated later so that it can change if methods change?
+
+paste0("Number of clusters in data: ",
+       length(unique(casper_2026$surv_cluster)))
+# Should be 29, one cluster will not appear because there are no willing HH
+
+# Calculate the weights
+casper_2026 <- casper_2026 %>%
+  group_by(surv_cluster) %>%
+  mutate(surv_weight = total.hh.sample / (n() * total.clusters)) %>%
+  ungroup()
+
+# Validation ===================================================================
+
+# Check for duplicate surveys, should be 0 rows
+dupe_surveys <- casper_2026 %>%
+  group_by(surv_cluster, surv_survey_num) %>%
+  filter(n() > 1)
+
+if (nrow(dupe_surveys) > 0) {
+  warning("At least two surveys share the same Cluster/Survey number combination.")
+  View(dupe_surveys)
+}
+
+# Check for mismatching household counts
+hh_count_off <- casper_2026 %>%
+  filter(demo_n_hh != demo_n_u2 + demo_n_2to17 + demo_n_18to64 + demo_n_65p)
+
+if (nrow(hh_count_off) > 0) {
+  warning("At least one survey has a mismatch between the total household members and the sum of members by age.")
+  View(hh_count_off)
+}
+
+# Output =======================================================================
+saveRDS(casper_2026, "./data/casper_2026_raw.rds")
--- code/02_clean_vars.R
+++ code/02_clean_vars.R
@@ -4,253 +4,135 @@
 # Create indicator variables
 
 library(tidyverse)
+library(lubridate)
 
 # Import =======================================================================
 casper_2026 <- readRDS("./data/casper_2026_raw.rds")
 
 # Cleaning =====================================================================
-# Clean variables ---------------------------------------------------------
-# Substitute 0s with "No" for at least one member in each group
-# Create a validation variable for if Total_Hh_Members = SUM(Age groups)
-hurricane_vars <- c("Cat1", "Cat2", "Cat3", "Cat4", "Cat5")   #Q10
-
-casper_2026_clean <- casper_2026 %>%
-  mutate(
-    Infants_2 = if_else(as.numeric(Infants) > 0, "Yes", "No", missing = NA_character_),
-    Kids_2    = if_else(as.numeric(Kids) > 0, "Yes", "No", missing = NA_character_ ),
-    Adults_2  = if_else(as.numeric(Adults) > 0, "Yes", "No", missing = NA_character_),
-    Elders_2  = if_else(as.numeric(Elders) > 0, "Yes", "No", missing = NA_character_), 
-    
-    HHMembers_by_age = as.numeric(Infants) + as.numeric(Kids) + 
-      as.numeric(Adults) + as.numeric(Elders),
-    
-    HHMember_match = case_when(
-      is.na(Total_Hh_Members) | is.na(HHMembers_by_age) ~ NA_character_,
-      Total_Hh_Members == HHMembers_by_age ~ "Match",
-      TRUE ~ "Mismatch"),
-    
-    Cat5 = case_when(
-      Cat5 == "Shelter in place in at home" ~ "Shelter in place at home",
-      TRUE ~ Cat5),
-    
-    across(all_of(hurricane_vars), str_to_sentence))
-
-
-table(casper_2026_clean$HHMember_match) #investigate mismatches
-look <- casper_2026_clean %>%
-  select(Cluster_Number, Survey_Number, Total_Hh_Members, Infants_2,
-         Kids_2, Adults_2, Elders_2, HHMembers_by_age, HHMember_match) %>% 
-  filter(HHMember_match == 'Mismatch')
-
-# Substitute Language = Other with Language_Other 
-casper_2026_clean <- casper_2026_clean %>% 
-  mutate(Language_Other = str_to_title(Language_Other), #standardize response formatting
-         Language_Combined = case_when(
-           Language == "Other" & !is.na(Language_Other) ~ Language_Other,
-           Language == "Other" & is.na(Language_Other) ~ "Other (unspecified)",
-           TRUE ~ Language
-         ))
-
-table(casper_2026_clean$Language_Combined)
-table(casper_2026_clean$Language_Other) #look at other languages and clean
-
-# creating yes/no variable for each language
-casper_2026_clean <- casper_2026_clean %>% 
-  mutate(Ilocano = case_when(str_detect(Language_Other, "Ilocano|Ilokano|Ilikano|Ulakono|Ilikano") ~ 'Yes', TRUE ~ "No"),
-         Tagalog = case_when(str_detect(Language_Other, "Tagalog|Filipino|Tagalo") ~ 'Yes', TRUE ~ "No"),
-         Japanese = case_when(str_detect(Language_Other, "Japanese") ~ "Yes", TRUE ~ "No"),
-         Spanish = case_when(str_detect(Language_Other, "Spanish") ~ "Yes", TRUE ~ "No"),
-         Pidgin = case_when(str_detect(Language_Other, "Pidgin|Pigeon") ~ "Yes", TRUE ~ "No"),
-         Hawaiian = case_when(str_detect(Language_Other, "Hawaiian|Hawaiin") ~ "Yes", TRUE ~ "No"),
-         Visayan = case_when(str_detect(Language_Other, "Vizian|Visaya") ~ "Yes", TRUE ~ "No"),
-         Thai = case_when(str_detect(Language_Other, "Thai") ~ "Yes", TRUE ~ "No"),
-         Mandarin = case_when(str_detect(Language_Other, "Chinese Mandarin|Chinese") ~ "Yes", TRUE ~ "No"),
-         Korean = case_when(str_detect(Language_Other, "Korean") ~ "Yes", TRUE ~ "No"),
-         Laotian = case_when(str_detect(Language_Other, "Laotian") ~ "Yes", TRUE ~ "No"),
-         French = case_when(str_detect(Language_Other, "French") ~ "Yes", TRUE ~ "No"),
-         Greek = case_when(str_detect(Language_Other, "Greek") ~ "Yes", TRUE ~ "No")
-  )
-
-# Create new numeric alternative for likert questions
-casper_2026_clean <- casper_2026_clean %>%  
-  mutate(Community_Connected_2 = case_when(
-    str_detect(Community_Connected, "1") ~ 1,
-    str_detect(Community_Connected, "2") ~ 2,
-    str_detect(Community_Connected, "3") ~ 3,
-    str_detect(Community_Connected, "4") ~ 4,
-    str_detect(Community_Connected, "5") ~ 5,
-    TRUE                                 ~ NA  # Default value if none of the above are found
-  ))
-
-table(casper_2026_clean$Community_Connected_2) #check to see if there are any 0s
-
-likert_vars <- c("Phys_Health", "Ment_Health", "Community_Connected") #Q23a,b,24
-
-casper_2026_clean <- casper_2026_clean %>%
-  mutate(across(
-    all_of(likert_vars),
-    ~ case_when(
-      .x %in% c("1", "2", "3", "4", "5") ~ as.numeric(.x),
-      .x %in% c("Unsure", "Declined") ~ NA_real_
-    ),
-    .names = "{.col}_num"
-  ))
-
-# Factor Categorical -----------------------------------------------------------
-# LIST ALL VARIABLES WITH YES/NO/UNSURE/DECLINED ANSWERS
-yesno_vars <- c(#"Infants", "Kids", "Adults", "Elders",                         #Q1 - Not true, these are numeric answers
-                "Insurance_Loss",                                               #Q3b 
-                "Comm_Plan", "Meeting_Place", "Important_Docs",                 #Q4a,b,c
-                "Med_Equip", "Backup_Power",                                    #Q5a,b
-                "Tsunami_Zone_Familiar", "Tsunami_Zone_Info",                   #Q6a,b
-                "Wildfires_Protection", "Receive_Alerts", "Kema_Familiar",      #Q7,Q8,Q9
-                "Measles_Aware", "Chickenpox_Aware",                            #Q13,Q15
-                "Skip_Med_Care",                                                #Q22
-                "Food_Support", "Naloxone", "Concerns_Needs")                   #Q29,Q30,Q31
-
-#this orders the values to make visualization easier
-casper26_fmtd <- casper_2026_clean %>%
-  mutate(across(all_of(yesno_vars), 
-                ~factor(.x, levels = c("Yes", "No", "Unsure", "Declined"))))
-
-# HURRICANE CATEGORIES
-hurricane_vars <- c("Cat1", "Cat2", "Cat3", "Cat4", "Cat5")                     #Q10
-
-casper26_fmtd <- casper26_fmtd %>%
-  mutate(across(all_of(hurricane_vars), 
-                ~factor(.x, levels = c("Shelter in place at home",
-                                       "Friend/family's home",
-                                       "Public shelter",
-                                       "Workplace",
-                                       "Other",
-                                       "Unsure",
-                                       "Declined"))))
-
-#LIKERT
-likert_vars <- c("Phys_Health", "Ment_Health", "Community_Connected")           #Q23a, 23b, 24
-
-casper26_fmtd <- casper26_fmtd %>%
-  mutate(across(all_of(likert_vars), 
-                ~factor(.x, levels = c("1",
-                                       "2",
-                                       "3",
-                                       "4",
-                                       "5",
-                                       "Unsure",
-                                       "Declined"))))
-
-#OTHERS
-casper26_fmtd$Own_Rent = factor(                                                #Q3a
-  casper26_fmtd$Own_Rent,
-  levels = c("Own", "Rent","Other", "Unsure", "Declined"))
-casper26_fmtd$Additional_Evac_Needs = factor(                                   #Q12
-  casper26_fmtd$Additional_Evac_Needs,
-  levels = c("Yes", "No", "Unsure", "Declined", "Not Applicable"))
-casper26_fmtd$Measles_Concern = factor(                                         #Q14
-  casper26_fmtd$Measles_Concern,
-  levels = c("Very concerned", 
-             "Somewhat concerned",
-             "Not concerned",
-             "Unsure",
-             "Declined"))
-casper26_fmtd$Chickenpox_Concern = factor(                                      #Q16
-  casper26_fmtd$Chickenpox_Concern,
-  levels = c("Very concerned",
-             "Somewhat concerned",
-             "Not concerned",
-             "Unsure",
-             "Declined"))
-casper26_fmtd$Vaccines_Important = factor(                                      #Q17
-  casper26_fmtd$Vaccines_Important,
-  levels = c("Very important",
-             "Somewhat important",
-             "Not important",
-             "Unsure",
-             "Declined"))
-casper26_fmtd$Mosquito_Concern = factor(                                        #Q18
-  casper26_fmtd$Mosquito_Concern,
-  levels = c("Very concerned",
-             "Somewhat concerned",
-             "Not concerned",
-             "Unsure",
-             "Declined"))
-casper26_fmtd$Mosquito_Breeding = factor(                                       #Q19
-  casper26_fmtd$Mosquito_Breeding,
-  levels = c("Daily",
-             "Weekly",
-             "Monthly",
-             "A few times per year",
-             "Once a year",
-             "Never",
-             "Unsure",
-             "Declined"))
-casper26_fmtd$Specific_Doctor = factor(                                         #Q20
-  casper26_fmtd$Specific_Doctor,
-  levels = c("All",
-             "Most",
-             "Some",
-             "None",
-             "Unsure",
-             "Declined"))
-casper26_fmtd$Provider_Within_Year = factor(                                    #Q21
-  casper26_fmtd$Provider_Within_Year,
-  levels = c("All",
-             "Most",
-             "Some",
-             "None",
-             "Unsure",
-             "Declined"))
-casper26_fmtd$Often_Gather = factor(                                            #Q25
-  casper26_fmtd$Often_Gather,
-  levels = c("Daily",
-             "A few times per week",
-             "Weekly",
-             "A few times per month",
-             "Rarely",
-             "Never",
-             "Unsure",
-             "Declined"))
-casper26_fmtd$Often_Groups = factor(                                            #Q26
-  casper26_fmtd$Often_Groups,
-  levels = c("Daily",
-             "A few times per week",
-             "Weekly",
-             "A few times per month",
-             "Rarely",
-             "Never",
-             "Unsure",
-             "Declined"))
-casper26_fmtd$Rent_Concern = factor(                                            #Q27
-  casper26_fmtd$Rent_Concern,
-  levels = c("Very concerned",
-             "Somewhat concerned",
-             "Not concerned",
-             "Unsure",
-             "Declined"))
-casper26_fmtd$Food_Last = factor(                                               #Q28
-  casper26_fmtd$Food_Last,
-  levels = c("Always",
-             "Usually",
-             "Sometimes",
-             "Rarely",
-             "Never",
-             "Unsure",
-             "Declined"))
+casper_clean <- casper_2026
 
 # Indicator Variables ----------------------------------------------------------
-# Keiki = HH with kids under 18
-casper26_fmtd <- casper26_fmtd %>%
+# Ages
+casper_clean <- casper_clean %>%
   mutate(
-    keiki = if_else(
-      Infants > 0 | Kids > 0,
-      "Yes",
-      "No",
-      missing = NA_character_
+    demo_hh_infants = if_else(demo_n_u2 > 0, "Yes", "No", missing = NA_character_),
+    demo_hh_kids = if_else(demo_n_2to17 > 0, "Yes", "No", missing = NA_character_),
+    demo_hh_adults = if_else(demo_n_18to64 > 0, "Yes", "No", missing = NA_character_),
+    demo_hh_elders = if_else(demo_n_65p > 0, "Yes", "No", missing = NA_character_)
+  )
+
+# Languages
+# table(casper_2026$demo_language_other)
+
+# Create indicators with search, then create a clean, combined variable
+casper_clean <- casper_clean %>%
+  # All lowercase for searching
+  mutate(demo_language = str_to_lower(demo_language),
+         demo_language_other = str_to_lower(demo_language_other)) %>%
+  # Indicator column for each language
+  # TODO: should this be logical?
+  mutate(
+    demo_lang_english = if_else(
+      str_detect(demo_language, "english"), "Yes", "No", missing = "No"),
+    demo_lang_burmese = if_else(
+      str_detect(demo_language_other, "burmese"), "Yes", "No", missing = "No"),
+    demo_lang_french = if_else(
+      str_detect(demo_language_other, "french"), "Yes", "No", missing = "No"),
+    demo_lang_hawaiian = if_else(
+      str_detect(demo_language_other, "hawaii"), "Yes", "No", missing = "No"),
+    demo_lang_japanese = if_else(
+      str_detect(demo_language_other, "japanese"), "Yes", "No", missing = "No"),
+    demo_lang_korean = if_else(
+      str_detect(demo_language_other, "korean"), "Yes", "No", missing = "No"),
+    demo_lang_marshallese = if_else(
+      str_detect(demo_language_other, "marshallese"), "Yes", "No", missing = "No"),
+    demo_lang_pidgin = if_else(
+      str_detect(demo_language_other, "pidgin|pigeon"), "Yes", "No", missing = "No"),
+    demo_lang_filipino = if_else(
+      str_detect(demo_language_other, "filipino|tagalog"), "Yes", "No", missing = "No"),
+    demo_lang_pohnpeian = if_else(
+      str_detect(demo_language_other, "pohnpeian"), "Yes", "No", missing = "No"),
+    demo_lang_portuguese = if_else(
+      str_detect(demo_language_other, "portuguese"), "Yes", "No", missing = "No"),
+    demo_lang_spanish = if_else(
+      str_detect(demo_language_other, "spanish"), "Yes", "No", missing = "No"),
+    demo_lang_german = if_else(
+      str_detect(demo_language_other, "german"), "Yes", "No", missing = "No"),
+    demo_lang_ilocano = if_else(
+      str_detect(demo_language_other, "ilocano"), "Yes", "No", missing = "No")
+  ) %>%
+  mutate(
+    demo_langs = apply(
+      select(., starts_with("demo_lang_")),
+      1,
+      function(row) {
+        paste(
+          str_to_title(str_remove(names(row), "demo_lang_"))[row == "Yes"],
+          collapse = ", "
+          )
+      }
     )
   )
 
-table(casper26_fmtd$keiki, useNA="ifany")
+# Factor Yes/No Variables ------------------------------------------------------
+# Correct Declined to Decline
+casper_clean <- casper_clean %>%
+  mutate(across(everything(), ~str_replace(.x, "Decline$", "Declined")))
+
+# Get Yes/No columns
+yesno_options <- c("Yes", "No", "Unsure", "Declined")
+yesno_cols <- names(casper_clean)[
+  sapply(names(casper_clean), 
+         function(col){
+           all(casper_clean[[col]] %in% yesno_options)
+          }
+         )
+  ]
+
+casper_clean <- casper_clean %>%
+  mutate(across(all_of(yesno_cols), ~factor(.x, levels = yesno_options)))
+
+# Date Times -------------------------------------------------------------------
+datetime_cols <- names(casper_clean)[endsWith(names(casper_clean), "datetime")]
+
+casper_clean <- casper_clean %>%
+  mutate(across(all_of(datetime_cols), ~with_tz(.x, tzone = "HST")))
+
+# Factor Likert Variables ------------------------------------------------------
+likert_options <- c("1", "2", "3", "4", "5", "Unsure", "Declined")
+likert_cols <- c("supp_vax_important", "hlth_physical", "hlth_mental")
+
+casper_clean <- casper_clean %>%
+  mutate(across(all_of(likert_cols), ~factor(.x, levels = likert_options)))
+
+# Hurricane Variables ----------------------------------------------------------
+hur_options <- c("Shelter in place at home", "Friend/family's home",
+                  "Public shelter", "Other", "Unsure", "Declined")
+hur_cols <- paste0("evac_hur_cat", 1:5)
+
+casper_clean <- casper_clean %>%
+  mutate(across(all_of(hur_cols), ~factor(.x, levels = hur_options)))
+
+# Others -----------------------------------------------------------------------
+casper_clean$demo_own_rent = factor(
+  casper_clean$demo_own_rent,
+  levels = c("Own", "Rent", "Other", "Unsure", "Declined")
+)
+
+casper_clean$supp_mosquito_prevent = factor(
+  casper_clean$supp_mosquito_prevent,
+  levels = c("Daily", "Weekly", "Monthly", "A few times per year", "Once a year",
+             "Never", "Unsure", "Declined")
+)
+
+casper_clean$hlth_food_ran_out = factor(
+  casper_clean$hlth_food_ran_out,
+  levels = c("Always", "Usually", "Sometimes", "Rarely", "Never", "Unsure",
+             "Declined")
+)
 
 # Output =======================================================================
-saveRDS(casper26_fmtd, "./data/casper_2026_clean.rds")
-
+saveRDS(casper_clean %>%
+          select(starts_with(c("surv_", "demo_", "NDexp_", "evac_", "supp_", "hlth_"))),
+        "./data/casper_2026_clean.rds")
