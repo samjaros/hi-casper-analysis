d24587bc197d3d28599971e621d68adbf6fb509e
Sam Jaros <Samuel.Jaros.NSW@doh.hawaii.gov>
2026-09-23 16:42:33 -1000

    Clean - Fix household mismatches - Add missing language - Add more useful language breakdown - Fix issues with Y/N questions missing - All factor variables are factoring

--- code/02_clean_vars.R
+++ code/02_clean_vars.R
@@ -9,8 +9,40 @@ library(lubridate)
 # Import =======================================================================
 casper_2026 <- readRDS("./data/casper_2026_raw.rds")
 
-# Cleaning =====================================================================
-casper_clean <- casper_2026
+# Fix Invalid Obs ==============================================================
+
+# Mismatch household count -----------------------------------------------------
+# If there is no mismatch, all good
+# If the HH total is 1, randomly assign adult or elder
+# All other mismatches leave alone (for now)
+set.seed(123)
+casper_fix <- casper_2026 %>%
+  # Define "wrong" surveys & provide a random number for coin flips
+  mutate(
+    t_wrong_1 = 
+      (demo_n_hh != demo_n_u2 + demo_n_2to17 + demo_n_18to64 + demo_n_65p) &
+      (demo_n_hh == 1),
+    t_rand = runif(n())
+  ) %>%
+  # Define what to do with the "wrong" surveys
+  mutate(
+    demo_n_u2 = if_else(t_wrong_1, 0, demo_n_u2),
+    demo_n_2to17 = if_else(t_wrong_1, 0, demo_n_2to17),
+    demo_n_18to64 = case_when(
+      t_wrong_1 & t_rand < 0.5  ~ 1,
+      t_wrong_1 & t_rand >= 0.5 ~ 0,
+      T                         ~ demo_n_18to64
+    ),
+    demo_n_65p = case_when(
+      t_wrong_1 ~ 1 - demo_n_18to64,
+      T         ~ demo_n_65p
+    )
+  ) %>%
+  # Remove temporary variables
+  select(-starts_with("t_"))
+
+# Data Wrangling ===============================================================
+casper_clean <- casper_fix
 
 # Indicator Variables ----------------------------------------------------------
 # Ages
@@ -28,10 +60,11 @@ casper_clean <- casper_clean %>%
 # Create indicators with search, then create a clean, combined variable
 casper_clean <- casper_clean %>%
   # All lowercase for searching
-  mutate(demo_language = str_to_lower(demo_language),
-         demo_language_other = str_to_lower(demo_language_other)) %>%
+  mutate(
+    demo_language = str_to_lower(demo_language),
+    demo_language_other = str_to_lower(demo_language_other)
+  ) %>%
   # Indicator column for each language
-  # TODO: should this be logical?
   mutate(
     demo_lang_english = if_else(
       str_detect(demo_language, "english"), "Yes", "No", missing = "No"),
@@ -60,7 +93,9 @@ casper_clean <- casper_clean %>%
     demo_lang_german = if_else(
       str_detect(demo_language_other, "german"), "Yes", "No", missing = "No"),
     demo_lang_ilocano = if_else(
-      str_detect(demo_language_other, "ilocano"), "Yes", "No", missing = "No")
+      str_detect(demo_language_other, "ilocano"), "Yes", "No", missing = "No"),
+    demo_lang_kosrae = if_else(
+      str_detect(demo_language_other, "kosrae"), "Yes", "No", missing = "No")
   ) %>%
   mutate(
     demo_langs = apply(
@@ -75,20 +110,41 @@ casper_clean <- casper_clean %>%
     )
   )
 
-# TODO: Probably a more useful language breakdown
-# Maybe
-#   - No English
-#   - Only English
-#   - English and at least 1 other
-# Should Hawaiian be tracked at all?
+# Create language breakdown
+#  - Only English
+#  - English + at least 1 other
+#  - No English (just put the language(s) they speak)
+casper_clean <- casper_clean %>%
+  mutate(demo_langs_cat = 
+           case_when(
+             demo_langs == "English"           ~ "Only English",
+             str_detect(demo_langs, "English") ~ "English & 1+ Other",
+             T                                 ~ demo_langs
+           ))
+
+# Language Data Checks
+# First 2 columns have the raw data, second two have processed data
+casper_clean %>%
+  select(c(starts_with("demo_lang"), -starts_with("demo_lang_"))) %>%
+  distinct() %>%
+  View("Lang Processed Check")
+
+# First 2 columns have the raw data, rest have indicators. Every language should be indicated
+casper_clean %>%
+  select(c(starts_with("demo_language"), starts_with("demo_lang_"))) %>%
+  distinct() %>%
+  View("Lang Indicator Check")
 
 # Natural disaster experience --------------------------------------------------
 # Get data as indicator columns
+# TODO: Should these be NA if they "weren't affected" by a disaster?
 casper_clean <- casper_clean %>%
   # Keep raw data in separate column
   # "Other" data could be overwritten, so rename
-  mutate(NDexp_types = NDexp_type,
-         NDexp_type_other_ans = NDexp_type_other) %>%
+  mutate(
+    NDexp_types = NDexp_type,
+    NDexp_type_other_ans = NDexp_type_other
+  ) %>%
   # Get longer version of data
   separate_longer_delim(
     cols = NDexp_type,
@@ -124,14 +180,15 @@ casper_clean <- casper_clean %>%
   mutate(across(where(is.character), ~str_replace(.x, "Decline$", "Declined")))
 
 # Get Yes/No columns
-yesno_options <- c("Yes", "No", "Unsure", "Declined")
+yesno_options <- c("Yes", "No", "Unsure", "Declined", NA_character_)
 yesno_cols <- names(casper_clean)[
-  sapply(names(casper_clean), 
-         function(col){
-           all(casper_clean[[col]] %in% yesno_options)
-          }
-         )
-  ]
+  sapply(
+    names(casper_clean), 
+    function(col) {
+      all(casper_clean[[col]] %in% yesno_options) & !all(is.na(casper_clean[[col]]))
+    }
+  )
+]
 
 casper_clean <- casper_clean %>%
   mutate(across(all_of(yesno_cols), ~factor(.x, levels = yesno_options)))
@@ -150,32 +207,74 @@ casper_clean <- casper_clean %>%
   mutate(across(all_of(likert_cols), ~factor(.x, levels = likert_options)))
 
 # Hurricane Variables ----------------------------------------------------------
-hur_options <- c("Shelter in place at home", "Friend/family's home",
-                  "Public shelter", "Other", "Unsure", "Declined")
+hur_options <- c(
+  "Shelter in place at home", "Friend/family's home", "Public shelter", "Other",
+  "Unsure", "Declined"
+)
 hur_cols <- paste0("evac_hur_cat", 1:5)
 
 casper_clean <- casper_clean %>%
   mutate(across(all_of(hur_cols), ~factor(.x, levels = hur_options)))
 
 # Others -----------------------------------------------------------------------
-casper_clean$demo_own_rent = factor(
+casper_clean$demo_own_rent <- factor(
   casper_clean$demo_own_rent,
   levels = c("Own", "Rent", "Other", "Unsure", "Declined")
 )
 
-casper_clean$supp_mosquito_prevent = factor(
+casper_clean$supp_mosquito_prevent <- factor(
   casper_clean$supp_mosquito_prevent,
-  levels = c("Daily", "Weekly", "Monthly", "A few times per year", "Once a year",
-             "Never", "Unsure", "Declined")
+  levels = c(
+    "Daily", "Weekly", "Monthly", "A few times per year", "Once a year", 
+    "Never", "Unsure", "Declined"
+  )
 )
 
-casper_clean$hlth_food_ran_out = factor(
+casper_clean$hlth_food_ran_out <- factor(
   casper_clean$hlth_food_ran_out,
-  levels = c("Always", "Usually", "Sometimes", "Rarely", "Never", "Unsure",
-             "Declined")
+  levels = c(
+    "Always", "Usually", "Sometimes", "Rarely", "Never", "Unsure", "Declined"
+  )
+)
+
+casper_clean$evac_main_barrier <- factor(
+  casper_clean$evac_main_barrier,
+  levels = c(
+    "Uncertainty about where to go", "Concern about leaving pet(s)",
+    "Concern about leaving property vacant", "Lack of transportation",
+    "Health or mobility issues", "Inconvenient or expensive", "Other",
+    "No barriers - household will evacuate", 
+    "No barriers - household would choose not to evacuate",
+    "Unsure", "Declined"
+  )
+)
+
+casper_clean$NDexp_damage <- factor(
+  casper_clean$NDexp_damage,
+  levels = c(
+    "None/Minimal", "Damaged, but repairable", "Destroyed", "Unsure", "Declined"
+  )
+)
+
+casper_clean$NDexp_prepared <- factor(
+  casper_clean$NDexp_prepared,
+  levels = c(
+    "Not Prepared", "Somewhat Prepared", "Well Prepared", "Unsure", "Declined"
+  )
+)
+
+casper_clean$evac_main_info_source <- factor(
+  casper_clean$evac_main_info_source,
+  levels = c(
+    "Social media", "Radio or TV", "Government website", 
+    "Community, friends, or family", "Newspaper or magazines", 
+    "Other", "Unsure", "Declined"
+  )
 )
 
 # Output =======================================================================
-saveRDS(casper_clean %>%
-          select(starts_with(c("surv_", "demo_", "NDexp_", "evac_", "supp_", "hlth_"))),
-        "./data/casper_2026_clean.rds")
+saveRDS(
+  casper_clean %>%
+    select(starts_with(c("surv_", "demo_", "NDexp_", "evac_", "supp_", "hlth_"))),
+  "./data/casper_2026_clean.rds"
+)
